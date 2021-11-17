e1e823aeff57ac3aa7ada58edddd79904b2782da
Hugo <hugolacerdar@gmail.com>
2021-11-16 21:22:24 -0300

    Add Auth Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import type { AppProps } from "next/app";
+
+export { AuthProvider } from "../contexts/AuthContext";
+
 import Header from "../components/Header";
 import Page from "../components/Page";
+import { AuthProvider } from "../contexts/AuthContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Page>
-      <Header />
-      <Component {...pageProps} />;
-    </Page>
+    <AuthProvider>
+      <Page>
+        <Header />
+        <Component {...pageProps} />;
+      </Page>
+    </AuthProvider>
   );
 }
 
