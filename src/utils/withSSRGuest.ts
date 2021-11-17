cd361cd504cdb22f95804afdbdd7906549dd0e39
Hugo <hugolacerdar@gmail.com>
2021-11-16 21:20:26 -0300

    Add SSR Guest service

---
+++ src/utils/withSSRGuest.ts
@@ -0,0 +1,24 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+export function withSSRGuest<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+    if (cookies["communicash.token"]) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
