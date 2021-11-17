9987d6a672f1fb017ef3b6abaa589bb95a66eb53
Hugo <hugolacerdar@gmail.com>
2021-11-16 21:22:55 -0300

    Add SSR conditional redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+
+import { setupAPIClient } from "../services/api";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 const Home: NextPage = () => {
-  return <div>HOME</div>;
+  return (
+    <div>
+      HOME
+      <p>
+        <Link href="/signin">Go to sign in page</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Home;
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get("users/profile");
+
+  return {
+    props: {},
+  };
+});
